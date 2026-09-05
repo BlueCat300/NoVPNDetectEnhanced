30b762cb5a59f8ce29191850ea632e22ee278ee3
Blue cat <bluecat300@gmail.com>
2026-09-06 02:02:23 +0300

    Update build to 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "ru.bluecat.novpndetectenhanced"
         minSdk = 26
         targetSdk = 37
-        versionCode = 13
-        versionName = "1.2"
+        versionCode = 18
+        versionName = "1.3"
 
         externalNativeBuild {
             cmake {
